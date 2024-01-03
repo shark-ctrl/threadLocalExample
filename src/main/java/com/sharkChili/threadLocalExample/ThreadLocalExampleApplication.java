49a0c1557a02e97617aa5877e9a168b9f5ce2c30
shark-chili <1946955309@qq.com>
2024-01-04 00:10:45 +0800

    init

---
+++ src/main/java/com/sharkChili/threadLocalExample/ThreadLocalExampleApplication.java
@@ -0,0 +1,13 @@
+package com.sharkChili.threadLocalExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadLocalExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadLocalExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sharkChili/threadLocalExample/ThreadLocalExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharkChili.threadLocalExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadLocalExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
